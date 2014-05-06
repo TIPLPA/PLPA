1b33032a5bd8b4a6f79599afc985f0dc8ae574fa
Rasmus Bækgaard <git@bakgaard.net>
2014-05-06 15:43:41 +0200

    Add existing project - can calculate small things but graph is not working

---
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using IronScheme;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateClick(object sender, RoutedEventArgs e)
+        {
+            dynamic input = CodeBox.Text.Eval();
+            var data = input.ToString();
+            MessageBox.Show(data);
+        }
+    }
+}
